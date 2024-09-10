8ba15d6943fbf4123e27071d34019a8da31f38eb
Doetzen <Doutzenhoekstra06@gmail.com>
2024-09-10 16:40:32 +0200

    startup

---
+++ Assets/Scripts/Bunny/Movement/Movement.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    private BunnyInput bunnyInput;
+    private InputAction move;
+
+
+}
