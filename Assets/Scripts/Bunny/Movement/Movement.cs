f486958bd149f0d46a1316b678aca1a1e565b7c5
Doetzen <Doutzenhoekstra06@gmail.com>
2024-09-11 13:49:36 +0200

    movement script

--- Assets/Scripts/Bunny/Movement/Movement.cs
+++ Assets/Scripts/Bunny/Movement/Movement.cs
@@ -2,11 +2,136 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.HID;
 
 public class Movement : MonoBehaviour
 {
     private BunnyInput bunnyInput;
-    private InputAction move;
+    private InputAction move, rotate;
+
+    public Rigidbody rb;
+    public Camera camHolder;
+
+    float  normalSpeed, maxSpeed;
+    public float jumpForce, sensitivity, moveSpeed;
+    float maxJumpForce;
+
+    public Vector3 forceDirection;
+    public Vector3 jump;
+
+    private Vector2 look;
+    private float x, y;
+
+
+    public bool isGrounded, jumping;
+
+    RaycastHit hit;
+
+    private void Awake()
+    {
+        bunnyInput = new BunnyInput();
+
+        jump = new Vector3 (0, 10, 0);
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+      // moveSpeed = normalSpeed;
+    }
+
+    private void OnEnable()
+    {
+        move = bunnyInput.Bunny.Move;
+        move.Enable();
+        rotate = bunnyInput.Bunny.Mouse;
+        bunnyInput.Bunny.Mouse.Enable();
+        rotate.Enable();
+    }
+
+    private void OnDisable()
+    {
+        move.Disable();
+        rotate.Disable();
+    }
+
+    public void DoJump(InputAction.CallbackContext context)
+    {
+        if (isGrounded && context.performed)
+        {
+             //movementAnimator.SetTrigger("Jumping");
+            jumping = true;
+            isGrounded = false;
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void Update()
+    {
+        Rotate();
+    }
+    private void FixedUpdate()
+    {
+        forceDirection += move.ReadValue<Vector2>().x * GetCameraRight(camHolder) * moveSpeed;
+        forceDirection += move.ReadValue<Vector2>().y * GetCameraForward(camHolder) * moveSpeed;
+
+        rb.AddForce(forceDirection, ForceMode.Impulse);
+        forceDirection = Vector3.zero;
+
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity -= Vector3.down * Physics.gravity.y * 3.5f * Time.deltaTime;
+        }
+
+
+        if (Physics.Raycast(transform.position, -Vector3.up, out hit, 0.1f))
+        {
+            if (hit.transform.tag != "Jumpable")
+            {
+                return;
+            }
+            if (!isGrounded)
+            {
+                isGrounded = true;
+                jumping = false;
+            }
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+        //animations
+        /*float actualSpeed = new Vector3(rb.velocity.x, rb.velocity.z, 0).magnitude;
+        movementAnimator.SetFloat("Actual Speed", actualSpeed);*/
+
+       
+    }
+
+    private Vector3 GetCameraForward(Camera cam)
+    {
+        Vector3 forward = cam.transform.forward;
+        forward.y = 0f;
+        return forward.normalized;
+    }
+    private Vector3 GetCameraRight(Camera cam)
+    {
+        Vector3 right = cam.transform.right;
+        right.y = 0f;
+        return right.normalized;
+    }
+
+    private void Rotate()
+    {
+        look = rotate.ReadValue<Vector2>();
+
+        x += look.x * sensitivity;
+        y -= look.y * sensitivity;
+
+        y = Mathf.Clamp(y, -85, 85);
+
+        transform.localRotation = Quaternion.Euler(0, x, 0 * Time.deltaTime);
+        camHolder.transform.localRotation = Quaternion.Euler(y, 0, 0 * Time.deltaTime);
+    }
 
 
 }
