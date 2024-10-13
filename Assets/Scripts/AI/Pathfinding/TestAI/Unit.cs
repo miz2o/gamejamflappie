163197c91599d169ab8b582b87deec218482be31
Dylano Burgers <dylano.burgers@outlook.com>
2024-10-13 21:47:50 +0200

    small bug fix
    did a small bug fix where i removed the psoudo code from the tuturials git hub and put my own code back in

--- Assets/Scripts/AI/Pathfinding/TestAI/Unit.cs
+++ Assets/Scripts/AI/Pathfinding/TestAI/Unit.cs
@@ -1,4 +1,4 @@
-/*using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -19,18 +19,18 @@ public class Unit : MonoBehaviour
 
     private void OnPathFound(Vector3[] newPath, bool pathSuccessfull)
     {
-        print("outside if");
-        //if(pathSuccessfull == true)
-       // {
+        if(pathSuccessfull == true)
+        {
             path = newPath;
-            print("in if");
+            
+
             targetIndex = 0;    
 
             StopCoroutine(FollowPath());
 
 
             StartCoroutine(FollowPath());
-       // }
+        }
     }
 
 
@@ -39,20 +39,15 @@ public class Unit : MonoBehaviour
     {
         Vector3 currentWaypoint = path[0];
 
-        print("inside ie numerator");
-
         while (true)
         {
-
-            print("inside while");
             if (transform.position == currentWaypoint)
             {
                 targetIndex++;
-                print("inside while if");
-
+ 
+                
                 if (targetIndex >= path.Length)
                 {
-                    print("inside while if if");
                     yield break;
                 }
 
@@ -67,57 +62,7 @@ public class Unit : MonoBehaviour
             yield return null;
         }
     }
-}
-*/
 
-using UnityEngine;
-using System.Collections;
-
-public class Unit : MonoBehaviour
-{
-
-
-    public Transform target;
-    float speed = 20;
-    Vector3[] path;
-    int targetIndex;
-
-    void Start()
-    {
-        PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
-    }
-
-    public void OnPathFound(Vector3[] newPath, bool pathSuccessful)
-    {
-        if (pathSuccessful)
-        {
-            path = newPath;
-            targetIndex = 0;
-            StopCoroutine("FollowPath");
-            StartCoroutine("FollowPath");
-        }
-    }
-
-    IEnumerator FollowPath()
-    {
-        Vector3 currentWaypoint = path[0];
-        while (true)
-        {
-            if (transform.position == currentWaypoint)
-            {
-                targetIndex++;
-                if (targetIndex >= path.Length)
-                {
-                    yield break;
-                }
-                currentWaypoint = path[targetIndex];
-            }
-
-            transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
-            yield return null;
-
-        }
-    }
 
     public void OnDrawGizmos()
     {
