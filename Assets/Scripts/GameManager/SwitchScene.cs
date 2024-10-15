2eec28731a168172c3296bba7c32ed2efbbe1729
Doetzen <Doutzenhoekstra06@gmail.com>
2024-10-15 16:57:20 +0200

    MainMenu and more prefabs

--- Assets/Scripts/GameManager/SwitchScene.cs
+++ Assets/Scripts/GameManager/SwitchScene.cs
@@ -5,8 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class SwitchScene : MonoBehaviour
 {
+    public GameObject highscoreMenu;
+
     public void ClickButton(string sceneToLoad)
     {
         SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void TurnMenuO()
+    {
+        if (highscoreMenu)
+        {
+            highscoreMenu.SetActive(false);
+        }
+        else
+        {
+            highscoreMenu.SetActive(true);
+        }
     }
 }
