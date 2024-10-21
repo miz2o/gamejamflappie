bfa693da9229e9e0c399535e6d15319979e6178a
Doetzen <Doutzenhoekstra06@gmail.com>
2024-10-21 14:49:41 +0200

    prefab changes

--- Assets/Scripts/PCG/TerrainGenerator/ObjectCheck.cs
+++ Assets/Scripts/PCG/TerrainGenerator/ObjectCheck.cs
@@ -1,25 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class ObjectCheck : MonoBehaviour
 {
-    RaycastHit hit;
-    Vector3 hitPoint;
+    //RaycastHit hit;
     private void Start()
     {
-        hitPoint = new Vector3(0,0.95f,0);
-        if (Physics.Raycast(transform.position, -Vector3.up, out hit, 10f))
+        /*if (Physics.Raycast(transform.position, -Vector3.up, out hit, 10f))
         {
-           // hitPoint = hit.point;
             Debug.Log("GROUNDED");
-            //this.transform.position = hit.point * hitPoint;
-        }
+            //this.transform.position = hit.point;
 
-       /* else
-        {
-            Debug.Log("Deleting object");
-            Destroy(this.gameObject);
+            //this.transform.Translate(hit.point);
+            //this.transform.position = hit.point * -Vector3.up * 1;
         }*/
+
+        Collider collider = gameObject.GetComponent<Collider>();
+
+        Vector3 lowestPoint = collider.bounds.min;
+        RaycastHit hit;
+        if (Physics.Raycast(lowestPoint + Vector3.up * 0.1f, Vector3.down, out hit))
+        {
+
+            float distanceToMoveDown = Vector3.Distance(lowestPoint, hit.point);
+            gameObject.transform.position -= Vector3.up * distanceToMoveDown;
+            Debug.Log("meeep");
+
+            /* else
+             {
+                 Debug.Log("Deleting object");
+                 Destroy(this.gameObject);
+             }*/
+        }
     }
 }
