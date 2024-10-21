2746a785439132b8f2a0b0f709d3dd88c5c906a2
Doetzen <Doutzenhoekstra06@gmail.com>
2024-10-21 16:05:18 +0200

    No more floating objects
    fixed the bug where we had floating objects. They now spawn correctly

--- Assets/Scripts/PCG/TerrainGenerator/ObjectCheck.cs
+++ Assets/Scripts/PCG/TerrainGenerator/ObjectCheck.cs
@@ -6,34 +6,22 @@ using UnityEngine;
 
 public class ObjectCheck : MonoBehaviour
 {
-    //RaycastHit hit;
+    RaycastHit hit;
     private void Start()
     {
-        /*if (Physics.Raycast(transform.position, -Vector3.up, out hit, 10f))
-        {
-            Debug.Log("GROUNDED");
-            //this.transform.position = hit.point;
+        Invoke("MoveObjects", 2);
+    }
 
-            //this.transform.Translate(hit.point);
-            //this.transform.position = hit.point * -Vector3.up * 1;
-        }*/
-
+    void MoveObjects()
+    {
         Collider collider = gameObject.GetComponent<Collider>();
 
         Vector3 lowestPoint = collider.bounds.min;
-        RaycastHit hit;
         if (Physics.Raycast(lowestPoint + Vector3.up * 0.1f, Vector3.down, out hit))
         {
-
             float distanceToMoveDown = Vector3.Distance(lowestPoint, hit.point);
             gameObject.transform.position -= Vector3.up * distanceToMoveDown;
             Debug.Log("meeep");
-
-            /* else
-             {
-                 Debug.Log("Deleting object");
-                 Destroy(this.gameObject);
-             }*/
         }
     }
 }
